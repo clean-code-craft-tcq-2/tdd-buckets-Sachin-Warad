6e6e849ad7fb1d715b2eb8cc0452f497fe3887fb
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-03-06 15:52:38 +0530

    Update getCurrentRangeAndOccurence.h

--- getCurrentRangeAndOccurence.h
+++ getCurrentRangeAndOccurence.h
@@ -1,4 +1,4 @@
 #include <stdio.h>
 #include <string.h>
 
-char* getCurrentRangeAndOccurence(int currentInputSamples[]);
+char* getCurrentRangeAndOccurence(int currentInputSamples[], int sampleSize);
