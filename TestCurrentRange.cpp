d4dc107cbb969226cecc82a6f14b1fce7e411ed2
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-03-06 14:00:41 +0530

    Update TestCurrentRange.cpp

--- TestCurrentRange.cpp
+++ TestCurrentRange.cpp
@@ -1,12 +1,13 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
+#include "getCurrentRangeAndOccurence.h"
 
 
 TEST_CASE("Infers the Current Ranges and their occurence") {
   int testCurrentInputSamples[] = {4,5};
   const char* expectedinterpretation = "4-5, 2";
   char* intrepretedRange;
-  intrepretedRange = getCurrentRangeAndOccurence(currentInputSamples);
+  intrepretedRange = getCurrentRangeAndOccurence(testCurrentInputSamples);
   REQUIRE(intrepretedRange == expectedinterpretation);
 }
