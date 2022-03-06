c4d9729e5b7d81e3ad3823d2cedab9f8dd6f9485
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-03-06 16:03:27 +0530

    Update getCurrentRangeAndOccurence.c

--- getCurrentRangeAndOccurence.c
+++ getCurrentRangeAndOccurence.c
@@ -1,8 +1,5 @@
 #include "getCurrentRangeAndOccurence.h"
 
-int getLowerCurrentLimit(int currentInputSamples[]);
-int getUpperCurrentLimit(int currentInputSamples[]);
-
 int getLowerCurrentLimit(int currentInputSamples[], int size) {
   int lowerLimit = currentInputSamples[0];
   for(int i=1; i<size; i++) {
@@ -19,24 +16,14 @@ int getUpperCurrentLimit(int currentInputSamples[], int size) {
   for(int i=1; i<size; i++) {
     if(upperLimit < currentInputSamples[i]) {
       upperLimit = currentInputSamples[i];
-      printf("UpperLimit In for %d\n",upperLimit);
     }
   }
-  printf("UpperLimit after for %d\n",upperLimit);
   return upperLimit;
 }
 
 char* getCurrentRangeAndOccurence(int currentInputSamples[], int sampleSize) {
-//   int temp[] = {4,5,6};
   char* buffer;
   int lowerCurrentLimit, upperCurrentLimit;
-//   int totalSize = sizeof(temp);
-//   int indiSize = sizeof(temp[0]);
-//   int size = sizeof(temp)/sizeof(temp[0]);
-//   for(int i=0; i<3; i++) {
-//     printf("%d\n", currentInputSamples[i]);
-//   }
-  printf("%d\n",sampleSize);
   lowerCurrentLimit = getLowerCurrentLimit(currentInputSamples, sampleSize);
   upperCurrentLimit = getUpperCurrentLimit(currentInputSamples, sampleSize);
   sprintf(buffer,"%d-%d, %d",lowerCurrentLimit,upperCurrentLimit,sampleSize);
