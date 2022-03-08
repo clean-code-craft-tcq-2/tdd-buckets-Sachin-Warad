8f563ff196e21b5c41d6650ad0fb5f2340f15ded
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-03-08 22:27:33 +0530

    Update TestCurrentRange.cpp

--- TestCurrentRange.cpp
+++ TestCurrentRange.cpp
@@ -7,9 +7,9 @@
 TEST_CASE("Infers the Current Ranges and their occurence") {
   int result;
   char* intrepretedRange;
-  int testCurrentInputSamples[] = {7,5,8,6,4,12,10,11};
+  int testCurrentInputSamples[] = {7,5,8,6,4,12,10,11,20};
   size_t sampleSize = sizeof(testCurrentInputSamples)/sizeof(testCurrentInputSamples[0]);
-  const char expectedinterpretation[50] = "4-8, 5\n10-12, 3\n";
+  const char expectedinterpretation[50] = "4-8, 5\n10-12, 3\n20, 1";
   intrepretedRange = getCurrentRangeAndOccurence(testCurrentInputSamples,sampleSize);
   result = strcmp(intrepretedRange, expectedinterpretation);
   REQUIRE(result == 0);
