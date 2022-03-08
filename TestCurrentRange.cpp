13817f45c3ebe6b41a43ac0d375e82f68e73e939
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-03-08 22:29:26 +0530

    Update TestCurrentRange.cpp
    Updated to handle single entry samples.
    test cases fails initially

--- TestCurrentRange.cpp
+++ TestCurrentRange.cpp
@@ -9,7 +9,7 @@ TEST_CASE("Infers the Current Ranges and their occurence") {
   char* intrepretedRange;
   int testCurrentInputSamples[] = {7,5,8,6,4,12,10,11,20};
   size_t sampleSize = sizeof(testCurrentInputSamples)/sizeof(testCurrentInputSamples[0]);
-  const char expectedinterpretation[50] = "4-8, 5\n10-12, 3\n20, 1";
+  const char expectedinterpretation[50] = "4-8, 5\n10-12, 3\n20, 1\n";
   intrepretedRange = getCurrentRangeAndOccurence(testCurrentInputSamples,sampleSize);
   result = strcmp(intrepretedRange, expectedinterpretation);
   REQUIRE(result == 0);
