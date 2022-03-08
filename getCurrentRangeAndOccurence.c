5f95f9f2ae652fec44ad3762685ad086904012ad
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-03-08 22:32:15 +0530

    Update getCurrentRangeAndOccurence.c

--- getCurrentRangeAndOccurence.c
+++ getCurrentRangeAndOccurence.c
@@ -5,8 +5,11 @@
 char* formatOutput(int consecutiveSamples[], int size) {
   char result[50];
   char *buffer = result;
-  sprintf(buffer,"%d-%d, %d\n",consecutiveSamples[0],consecutiveSamples[size-1],size);
-  //printf("%s",buffer);
+  if(size > 1) {
+    sprintf(buffer,"%d-%d, %d\n",consecutiveSamples[0],consecutiveSamples[size-1],size);
+  }else {
+    sprintf(buffer,"%d, %d\n",consecutiveSamples[0],size);
+  }
   return buffer;
 }
 
@@ -19,9 +22,7 @@ char* checkForConsecutiveSamples(int currentInputSamples[], int sampleSize) {
         if(currentInputSamples[i+1] - currentInputSamples[i] <= 1) {
             index++;
         }else {
-            //buffer = formatOutput(consecutiveSamples,(index+1));
             strcat(buffer,formatOutput(consecutiveSamples,(index+1)));
-            //printf("%s",buffer);
             index=0;
         }
     }
@@ -30,21 +31,19 @@ char* checkForConsecutiveSamples(int currentInputSamples[], int sampleSize) {
 }
 
 void sortCurrentRanges(int currentInputSamples[], int sampleSize) {
-    int i, j, temp;
-    for(i=0; i<sampleSize-1; i++) {
-        for(j=i+1; j<sampleSize; j++) {
-            if(currentInputSamples[i] > currentInputSamples[j]) {
-                temp = currentInputSamples[i];
-                currentInputSamples[i] = currentInputSamples[j];
-                currentInputSamples[j] = temp;
-            }
-        }
-    }
+  int i, j, temp;
+  for(i=0; i<sampleSize-1; i++) {
+      for(j=i+1; j<sampleSize; j++) {
+          if(currentInputSamples[i] > currentInputSamples[j]) {
+              temp = currentInputSamples[i];
+              currentInputSamples[i] = currentInputSamples[j];
+              currentInputSamples[j] = temp;
+          }
+      }
+  }
 }
 
 char* getCurrentRangeAndOccurence(int currentInputSamples[], size_t sampleSize) {
-//   char result[50];
-//   char *buffer = result;
   sortCurrentRanges(currentInputSamples, sampleSize);
   char *buffer = checkForConsecutiveSamples(currentInputSamples, sampleSize);
   return buffer;
