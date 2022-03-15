f8a53834c7893923df96c3b3db1e579a0a4dcb6a
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-03-16 00:43:26 +0530

    Update getCurrentRangeAndOccurence.h

--- getCurrentRangeAndOccurence.h
+++ getCurrentRangeAndOccurence.h
@@ -5,7 +5,7 @@ struct intrepetedData
     int Size;
 };
 
-int getCurrentRangeAndOccurence(int currentInputSamples[], size_t sampleSize, struct intrepetedData dataInterpreted[], void (*fn_ptrAlert)(int min, int max, int count));
+int getCurrentRangeAndOccurence(int currentInputSamples[], size_t sampleSize, struct intrepetedData dataInterpreted[], void (*fn_ptrPrintOutput)(int min, int max, int count));
 void sortCurrentRanges(int currentInputSamples[], int sampleSize);
 int checkForConsecutiveSamples(int currentInputSamples[], int sampleSize, struct intrepetedData dataInterpreted[]);
 void updateIntrepretedData(struct intrepetedData dataInterpreted[], int consecutiveSamples[], int size, int consecutiveSamplesNumber);
