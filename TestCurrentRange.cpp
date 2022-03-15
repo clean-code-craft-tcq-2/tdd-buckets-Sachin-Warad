2cbca2fbcaed63b695cc0b32a8b7ec4c82e26b92
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-03-16 01:24:48 +0530

    Update TestCurrentRange.cpp

--- TestCurrentRange.cpp
+++ TestCurrentRange.cpp
@@ -10,8 +10,19 @@ TEST_CASE("Infers the Current Ranges and their occurence") {
   struct intrepetedData dataInterpreted[sampleSize];
   void (*fn_ptrPrintOutput)(int, int, int);
   fn_ptrPrintOutput = &printOnConsole;
-  int expectedConsecutiveSamples = getCurrentRangeAndOccurence(testCurrentInputSamples,sampleSize,dataInterpreted,fn_ptrPrintOutput);
-  REQUIRE(expectedConsecutiveSamples == 3);
+  int expectedConsecutiveSamples = 3;
+  int intrepretedConsecutiveSamples = getCurrentRangeAndOccurence(testCurrentInputSamples,sampleSize,dataInterpreted,fn_ptrPrintOutput);
+  REQUIRE(intrepretedConsecutiveSamples == expectedConsecutiveSamples);
+  intrepetedData expectedData[sampleSize] = {
+    {4,8,5},
+    {10,12,3},
+    {20,20,1}
+  }
+  for(int i=0; i<(int)sampleSize; i++) {
+    REQUIRE(dataInterpreted[i].Min == expectedData[i].Min);
+    REQUIRE(dataInterpreted[i].Max == expectedData[i].Max);
+    REQUIRE(dataInterpreted[i].Size == expectedData[i].Size);
+  }
   
   int testCurrentInputSamples1[] = {6,-3,7,9,10,12};
   size_t sampleSize1 = sizeof(testCurrentInputSamples1)/sizeof(testCurrentInputSamples1[0]);
