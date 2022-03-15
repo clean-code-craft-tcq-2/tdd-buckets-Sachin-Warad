16dbfffbbc7e549800bf9ef3e3a623b8ca9a6295
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-03-15 23:51:06 +0530

    Update getCurrentRangeAndOccurence.h

--- getCurrentRangeAndOccurence.h
+++ getCurrentRangeAndOccurence.h
@@ -1,3 +1,10 @@
+struct IntrepetedData
+{
+    int Min;
+    int Max;
+    int Size;
+};
+
 char* getCurrentRangeAndOccurence(int currentInputSamples[], size_t sampleSize);
 void sortCurrentRanges(int currentInputSamples[], int sampleSize);
 char* checkForConsecutiveSamples(int currentInputSamples[], int sampleSize);
