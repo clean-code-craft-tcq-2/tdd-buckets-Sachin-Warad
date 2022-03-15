9c40115524197865e01df88fddfe1b987362e7d2
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-03-16 00:21:24 +0530

    Update TestCurrentRange.cpp

--- TestCurrentRange.cpp
+++ TestCurrentRange.cpp
@@ -8,6 +8,6 @@ TEST_CASE("Infers the Current Ranges and their occurence") {
   int testCurrentInputSamples[] = {7,5,8,6,4,12,10,11,20};
   size_t sampleSize = sizeof(testCurrentInputSamples)/sizeof(testCurrentInputSamples[0]);
   struct intrepetedData dataInterpreted[sampleSize];
-  int expectedConsecutiveSamples = getCurrentRangeAndOccurence(testCurrentInputSamples,sampleSize, );
+  int expectedConsecutiveSamples = getCurrentRangeAndOccurence(testCurrentInputSamples,sampleSize,dataInterpreted);
   REQUIRE(expectedConsecutiveSamples == 3);
 }
