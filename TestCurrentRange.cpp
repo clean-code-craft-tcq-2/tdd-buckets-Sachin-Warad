1ba29fe7e5f5043b3e44f0f375df17cf68e04f14
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-03-16 11:32:38 +0530

    Update TestCurrentRange.cpp

--- TestCurrentRange.cpp
+++ TestCurrentRange.cpp
@@ -8,12 +8,12 @@ TEST_CASE("Infers the Current Ranges and their occurence") {
   int testCurrentInputSamples1[] = {7,5,8,6,4,12,10,11,20,22};
   int expectedConsecutiveSamples1 = 4;
   size_t sampleSize1 = sizeof(testCurrentInputSamples1)/sizeof(testCurrentInputSamples1[0]);
-  struct intrepetedData dataInterpreted[sampleSize];
+  struct intrepetedData dataInterpreted[sampleSize1];
   void (*fn_ptrPrintOutput)(int, int, int);
   fn_ptrPrintOutput = &printOnConsole;
-  int intrepetedConsecutiveSamples1 = getCurrentRangeAndOccurence(testCurrentInputSamples1,sampleSize,dataInterpreted,fn_ptrPrintOutput);
+  int intrepetedConsecutiveSamples1 = getCurrentRangeAndOccurence(testCurrentInputSamples1,sampleSize1,dataInterpreted,fn_ptrPrintOutput);
   REQUIRE(expectedConsecutiveSamples1 == intrepetedConsecutiveSamples1);
-  intrepetedData expectedData[sampleSize] = {
+  intrepetedData expectedData[sampleSize1] = {
     {4,8,5},
     {10,12,3},
     {20,20,1},
@@ -27,7 +27,7 @@ TEST_CASE("Infers the Current Ranges and their occurence") {
   
   int testCurrentInputSamples2[] = {6,-3,7,9,10,12};
   int expectedConsecutiveSamples2 = 0;
-  size_t sampleSize1 = sizeof(testCurrentInputSamples2)/sizeof(testCurrentInputSamples2[0]);
+  size_t sampleSize2 = sizeof(testCurrentInputSamples2)/sizeof(testCurrentInputSamples2[0]);
   int intrepetedConsecutiveSamples2 = getCurrentRangeAndOccurence(testCurrentInputSamples2,sampleSize2,dataInterpreted,fn_ptrPrintOutput);
   REQUIRE(expectedConsecutiveSamples2 == intrepetedConsecutiveSamples2);
 }
