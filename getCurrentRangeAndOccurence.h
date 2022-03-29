5d2f5c0ac09f2a0ead4f555c4a62b3544609823f
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-03-29 20:48:23 +0530

    Update getCurrentRangeAndOccurence.h

--- getCurrentRangeAndOccurence.h
+++ getCurrentRangeAndOccurence.h
@@ -1,15 +1,15 @@
 struct intrepetedData
 {
-    int Min;
-    int Max;
+    double Min;
+    double Max;
     int Size;
 };
 
-int getCurrentRangeAndOccurence(int currentInputSamples[], size_t sampleSize, struct intrepetedData dataInterpreted[], void (*fn_ptrPrintOutput)(int min, int max, int count));
-int validateInputs(int currentInputSamples[], int sampleSize);
-int handleValidSampleCase(int currentInputSamples[], int sampleSize, struct intrepetedData dataInterpreted[]);
-void sortCurrentRanges(int currentInputSamples[], int sampleSize);
-int checkForConsecutiveSamples(int currentInputSamples[], int sampleSize, struct intrepetedData dataInterpreted[]);
-void updateIntrepretedData(struct intrepetedData dataInterpreted[], int consecutiveSamples[], int size, int consecutiveSamplesNumber);
-void printOnConsole(int min, int max, int count);
-void decryptingADC(double currentInputSamples[],int sampleSize);
+int getCurrentRangeAndOccurence(double currentInputSamples[], size_t sampleSize, struct intrepetedData dataInterpreted[], void (*fn_ptrPrintOutput)(double min, double max, int count));
+int validateInputs(double currentInputSamples[], int sampleSize);
+int handleValidSampleCase(double currentInputSamples[], int sampleSize, struct intrepetedData dataInterpreted[]);
+void sortCurrentRanges(double currentInputSamples[], int sampleSize);
+int checkForConsecutiveSamples(double currentInputSamples[], int sampleSize, struct intrepetedData dataInterpreted[]);
+void updateIntrepretedData(struct intrepetedData dataInterpreted[], double consecutiveSamples[], int size, int consecutiveSamplesNumber);
+void printOnConsole(double min, double max, int count);
+void convertToAmps(double currentInputSamples[],int sampleSize);
