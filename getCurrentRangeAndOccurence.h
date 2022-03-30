c24e45ebaf1a1ad1a78ae0264043c7c586239a23
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-03-30 07:26:43 +0530

    Update getCurrentRangeAndOccurence.h

--- getCurrentRangeAndOccurence.h
+++ getCurrentRangeAndOccurence.h
@@ -16,10 +16,11 @@ typedef enum {
 int getCurrentRangeAndOccurence(double currentInputSamples[], size_t sampleSize, struct intrepetedData dataInterpreted[], 
                                 void (*fn_ptrPrintOutput)(double min, double max, int count), SensorType sensor);
 int validateInputs(double currentInputSamples[], int sampleSize, SensorType sensor, double sensorMaxValueErr[]);
-int handleValidSampleCase(double currentInputSamples[], int sampleSize, struct intrepetedData dataInterpreted[]);
+int handleValidSampleCase(double currentInputSamples[], int sampleSize, struct intrepetedData dataInterpreted[], SensorType sensor);
 void sortCurrentRanges(double currentInputSamples[], int sampleSize);
 int checkForConsecutiveSamples(double currentInputSamples[], int sampleSize, struct intrepetedData dataInterpreted[]);
 void updateIntrepretedData(struct intrepetedData dataInterpreted[], double consecutiveSamples[], int size, int consecutiveSamplesNumber);
 void printOnConsole(double min, double max, int count);
-void convertToAmps(double currentInputSamples[],int sampleSize);
+void convertToAmps(double currentInputSamples[],int sampleSize, SensorType sensor);
+void convertToAmps12BitSesnor(double currentInputSamples[],int sampleSize);
 void convertToAmps10BitSesnor(double currentInputSamples[],int sampleSize);
